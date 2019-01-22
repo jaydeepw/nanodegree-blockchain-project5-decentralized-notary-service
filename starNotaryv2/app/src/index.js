141261b1362d155e542a1f7847b9d7af8a1cf401
jaydeepw <jaydeep.w@gmail.com>
2019-01-22 15:58:56 +0530

    Make the boilerplate project compile and run.

--- starNotaryv2/app/src/index.js
+++ starNotaryv2/app/src/index.js
@@ -1,76 +1,89 @@
-import Web3 from "web3";
-import metaCoinArtifact from "../../build/contracts/MetaCoin.json";
+// Import the page's CSS. Webpack will know what to do with it.
+import '../styles/app.css'
+
+// Import libraries we need.
+import { default as Web3 } from 'web3'
+import { default as contract } from 'truffle-contract'
+
+// Import our contract artifacts and turn them into usable abstractions.
+import StarNotaryArtifact from '../../build/contracts/StarNotary.json'
+
+// StarNotary is our usable abstraction, which we'll use through the code below.
+const StarNotary = contract(StarNotaryArtifact)
+
+let accounts
+let account
+
+const createStar = async () => {
+  const instance = await StarNotary.deployed();
+  const name = document.getElementById("starName").value;
+  const id = document.getElementById("starId").value;
+  await instance.createStar(name, id, {from: account});
+  App.setStatus("New Star Owner is " + account + ".");
+}
+
 
 const App = {
-  web3: null,
-  account: null,
-  meta: null,
+  start: function () {
+    const self = this
 
-  start: async function() {
-    const { web3 } = this;
+    // Bootstrap the MetaCoin abstraction for Use.
+    StarNotary.setProvider(web3.currentProvider)
 
-    try {
-      // get contract instance
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = metaCoinArtifact.networks[networkId];
-      this.meta = new web3.eth.Contract(
-        metaCoinArtifact.abi,
-        deployedNetwork.address,
-      );
+    // Get the initial account balance so it can be displayed.
+    web3.eth.getAccounts(function (err, accs) {
+      if (err != null) {
+        alert('There was an error fetching your accounts.')
+        return
+      }
 
-      // get accounts
-      const accounts = await web3.eth.getAccounts();
-      this.account = accounts[0];
+      if (accs.length === 0) {
+        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.")
+        return
+      }
 
-      this.refreshBalance();
-    } catch (error) {
-      console.error("Could not connect to contract or chain.");
-    }
+      accounts = accs
+      account = accounts[0]
+
+    })
   },
 
-  refreshBalance: async function() {
-    const { getBalance } = this.meta.methods;
-    const balance = await getBalance(this.account).call();
-
-    const balanceElement = document.getElementsByClassName("balance")[0];
-    balanceElement.innerHTML = balance;
+  setStatus: function (message) {
+    const status = document.getElementById('status')
+    status.innerHTML = message
   },
 
-  sendCoin: async function() {
-    const amount = parseInt(document.getElementById("amount").value);
-    const receiver = document.getElementById("receiver").value;
-
-    this.setStatus("Initiating transaction... (please wait)");
-
-    const { sendCoin } = this.meta.methods;
-    await sendCoin(receiver, amount).send({ from: this.account });
-
-    this.setStatus("Transaction complete!");
-    this.refreshBalance();
+  createStar: function () {
+    createStar();
   },
 
-  setStatus: function(message) {
-    const status = document.getElementById("status");
-    status.innerHTML = message;
-  },
-};
+}
 
-window.App = App;
+window.App = App
 
-window.addEventListener("load", function() {
-  if (window.ethereum) {
-    // use MetaMask's provider
-    App.web3 = new Web3(window.ethereum);
-    window.ethereum.enable(); // get permission to access accounts
+window.addEventListener('load', function () {
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+    console.warn(
+      'Using web3 detected from external source.' +
+      ' If you find that your accounts don\'t appear or you have 0 MetaCoin,' +
+      ' ensure you\'ve configured that source properly.' +
+      ' If using MetaMask, see the following link.' +
+      ' Feel free to delete this warning. :)' +
+      ' http://truffleframework.com/tutorials/truffle-and-metamask'
+    )
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider)
   } else {
     console.warn(
-      "No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",
-    );
+      'No web3 detected. Falling back to http://127.0.0.1:9545.' +
+      ' You should remove this fallback when you deploy live, as it\'s inherently insecure.' +
+      ' Consider switching to Metamask for development.' +
+      ' More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
+    )
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(
-      new Web3.providers.HttpProvider("http://127.0.0.1:9545"),
-    );
+    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'))
   }
 
-  App.start();
-});
+  App.start()
+})
